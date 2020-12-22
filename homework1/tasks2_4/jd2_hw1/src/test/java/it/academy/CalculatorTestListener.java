f9161a787938b8bc858a903e3bf856d96858b6e4
Aliaksandr <aliaksandr.talaj@gmail.com>
2020-12-22 21:08:01 +0300

    Refactored homework1

---
+++ homework1/tasks2_4/jd2_hw1/src/test/java/it/academy/CalculatorTestListener.java
@@ -0,0 +1,12 @@
+package it.academy;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunListener;
+
+public class CalculatorTestListener extends RunListener{
+
+    @Override
+    public void testFinished(Description description) throws java.lang.Exception {
+        System.out.println("Testing is finished: " + description.getMethodName());
+    }
+}
